7091b8c63d5c2c22cfbd2a3a8fc7575d19c47007
messi <messi@163.com>
2023-07-19 16:32:11 +0800

    init

---
+++ 01function/external/function.js
@@ -0,0 +1,28 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML="My First JavaScript Function" ;
+}
+
+function myFunction2() {
+    document.write("<h1>from write function</h1>") ;
+    document.write("<p>from write function</p>") ;
+}
+
+
+function myFunction3() {
+    document.getElementById("demo3").innerHTML="hello world";
+    document.getElementById("mydiv3").innerHTML="how are you guy";
+}
+
+
+function myFunction4() {
+    var x="messi";
+    var y=3;
+    var y1=2;
+    var z=y1+y;
+    z=z+x ;
+
+    document.write(x + "<br>") ;
+    document.write(y + "<br>") ;
+    document.write(z + "<br>") ;
+}
---
+++ 02var/var.js
@@ -0,0 +1,9 @@
+
+function varFunction() {
+
+    var x = true ;
+    var y = "hello" ; // 
+
+    document.getElementById("head").innerHTML=x ;
+    document.getElementById("Paragrph").innerHTML=y ;
+}
